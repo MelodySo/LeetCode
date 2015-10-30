6f0d4a2d116d89adafdc7b86be2cf7fdaa76800c
MelodySo <529419853@qq.com>
2015-10-30 17:57:48 +0800

    Solution 7

---
+++ Assets/7-Reverse Integer/ReverseIntegerBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using ReverseInteger;
+public class ReverseIntegerBehaviour : MonoBehaviour {
+	ReverseInteger.Solution solution=new ReverseInteger.Solution();
+	public int num;
+	void OnGUI(){
+		if (GUILayout.Button ("ReverseInteger:"+num)) {
+			Debug.Log(solution.Reverse(num));
+		}
+	}
+}
---
+++ Assets/7-Reverse Integer/ReverseIntegerSolution.cs
@@ -0,0 +1,20 @@
+﻿namespace ReverseInteger{
+	public class Solution {
+		public int Reverse(int x) {
+			int result = 0;
+			while (x != 0)
+			{
+				int tail = x % 10;
+				int newResult = result * 10 + tail;
+				if ((newResult - tail) / 10 != result)
+				{ 
+					return 0;
+				}
+				result = newResult;
+				x = x / 10;
+			}
+			
+			return result;
+		}
+	}
+}
