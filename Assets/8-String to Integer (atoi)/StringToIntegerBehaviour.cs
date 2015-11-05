4ed06743524b86a6ea1831cef905960027f60634
MelodySo <529419853@qq.com>
2015-11-05 17:14:25 +0800

    Solution 8 String To Integer

---
+++ Assets/8-String to Integer (atoi)/StringToIntegerBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using StringToInteger;
+public class StringToIntegerBehaviour : MonoBehaviour {
+	StringToInteger.Solution solution=new StringToInteger.Solution();
+	public string str;
+	void OnGUI(){
+		if (GUILayout.Button ("StringToInteger:"+str)) {
+			Debug.Log(solution.MyAtoi(str));
+		}
+	}
+}
---
+++ Assets/8-String to Integer (atoi)/StringToIntegerSolution.cs
@@ -0,0 +1,33 @@
+﻿namespace StringToInteger{
+	public class Solution {
+		public int MyAtoi(string str) {
+			int index = 0, sign = 1, total = 0;
+			//1. Empty string
+			if(str.Length == 0) return 0;
+			
+			//2. Remove Spaces
+			while(str[index] == ' ' && index < str.Length)
+				index ++;
+			
+			//3. Handle signs
+			if(str[index] == '+' || str[index] == '-'){
+				sign = str[index] == '+' ? 1 : -1;
+				index ++;
+			}
+			
+			//4. Convert number and avoid overflow
+			while(index < str.Length){
+				int digit = str[index] - '0';
+				if(digit < 0 || digit > 9) break;
+				
+				//check if total will be overflow after 10 times and add digit
+				if(int.MaxValue/10 < total || int.MaxValue/10 == total && int.MaxValue %10 < digit)
+					return sign == 1 ? int.MaxValue : int.MinValue;
+				
+				total = 10 * total + digit;
+				index ++;
+			}
+			return total * sign;
+		}
+	}
+}
