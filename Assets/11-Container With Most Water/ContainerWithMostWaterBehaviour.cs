de7d83feb17bac7175f5004003e4d8099f324f49
MelodySo <529419853@qq.com>
2015-11-06 16:46:15 +0800

    Solution 10,11
    10-Regular Expression Matching
    11-Container With Most Water

---
+++ Assets/11-Container With Most Water/ContainerWithMostWaterBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using ContainerWithMostWater;
+public class ContainerWithMostWaterBehaviour : MonoBehaviour {
+	ContainerWithMostWater.Solution solution=new ContainerWithMostWater.Solution();
+	public int[] nums;
+	void OnGUI(){
+		if (GUILayout.Button ("ContainerWithMostWater:"+CommonTool.ArrayToJsonString(nums))) {
+			Debug.Log(solution.MaxArea(nums));
+		}
+	}
+}
---
+++ Assets/11-Container With Most Water/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ContainerWithMostWater{
+	public class Solution {
+		public int MaxArea(int[] height) {
+			int len = height.Length, low = 0, high = len -1 ;  
+			int maxArea = 0;  
+			while (low < high) {  
+				maxArea = Math.Max(maxArea, (high - low) * Math.Min(height[low], height[high]));  
+				if (height[low] < height[high]) {  
+					low++;  
+				} else {  
+					high--;  
+				}  
+			}
+			return maxArea;  
+		}
+	}
+}
